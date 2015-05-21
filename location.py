2b527074b71362df2f1998c500ffc78b046acafc
SodaCookie <xzeric@hotmail.com>
2015-05-20 23:48:11 -0400

    Minor changes, Added file

--- location.py
+++ location.py
@@ -1,11 +1,19 @@
 from itertools import chain
 from random import choice
+import xml.etree.ElementTree as tree
 import dialog
 
 class Location(object):
   """Location object is the node for each part of a dungeon"""
+
+  ROOM_TYPE = {1 : ["bridge", "hallway", "tunnel"],\
+               2 : ["fork"],\
+               "default" : ["room"]}
+
   def __init__(self, loc_type, level):
     self.loc_type = loc_type # can be event, entrance, exit, shop, alter, item
+    self.description = "" # description is presented to the user after generation
+    self.tags = []
     self.level = level
     self.locked = False # to be implemented
     self.blocked = False # to be implemented
@@ -33,19 +41,24 @@ class Location(object):
   def get_neighbours(self):
     return self.neighbours
 
-  def traverse(self, index):
+  def traverse(self, index, tags=[]):
+    self.neighbours[index].tags.extend(tags)
     return self.neighbours[index]
 
   def generate(self, etree):
-    """Method is used to generate event based off the situation of the node"""
-    possible_dialogs = list(chain(etree.findall("event[@type='%s'][@level='%s']"%(self.loc_type, self.level)),\
-                                            etree.findall("event[@type='%s'][@level='%s']"%(loc_type, "any"))))
+    """Method is used to generate event and description based off the situation of the node"""
+    possible_dialogs = list(chain(etree.findall("event[@type='%s'][@level='%s']"%(self.loc_type, self.level)), etree.findall("event[@type='%s'][@level='%s']"%(self.loc_type, "any"))))
     self.event = choice(possible_dialogs)
     # this method will use the number of incoming nodes, out going nodes, adjacent tags and tags to determine the description
+    if Location.ROOM_TYPE.get(len(self.get_neighbours())) != None:
+      self.tags.append("type@%s"%choice(Location.ROOM_TYPE[len(self.get_neighbours())]))
+    else:
+      self.tags.append("type@%s"%choice(Location.ROOM_TYPE["default"]))
+    self.tags.extend(self.event.find("tag").text.split(' '))
+
 
 if __name__ == "__main__":
-  import xml.etree.ElementTree as tree
   #from lxml import etree as tree #optional version of the tree in case we need faster parsing
   with open("data/test_data.xml", "r") as file:
     etree = tree.parse(file)
-  loc = Location("event", "test", etree)
+  loc = Location("event", "test")
