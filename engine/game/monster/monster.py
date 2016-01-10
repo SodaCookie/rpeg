13ad926ba12f569e59d10413d624d947919b6ed3
SodaCookie <xzeric@hotmail.com>
2016-01-09 21:01:22 -0500

    Updated/Added action commands battle and loot

--- engine/game/monster/monster.py
+++ engine/game/monster/monster.py
@@ -8,7 +8,6 @@ from pygame.transform import scale
 
 import engine.game.character.character as character
 
-from engine.game.monster.general_builder import GeneralBuilder
 
 def parse_monsters(filename):
     """Takes a filename and returns a dict of dicts containing all monster
@@ -21,7 +20,7 @@ def parse_monsters(filename):
         monsters[name]['location'] = monster.find('location').text
         monsters[name]['stats'] = {}
         for stat in monster.find('stats'):
-            monsters[name]['stats'][stat.tag] = stat.text
+            monsters[name]['stats'][stat.tag] = int(stat.text)
         monsters[name]['abilities'] = []
         for ability in monster.find('abilities'):
             monsters[name]['abilities'].append(ability.text)
@@ -29,214 +28,22 @@ def parse_monsters(filename):
         for attribute in monster.find('attributes'):
             monsters[name]['attributes'].append(attribute.text)
         monsters[name]['graphic'] = {} # we need to do some work here
-        monsters[name]['rating'] = monster.find('rating').text
+        monsters[name]['rating'] = int(monster.find('rating').text)
     return monsters
-
-def parse_tags(filename):
-    """Takes a filename and returns a list of all possible tags."""
-    root = tree.parse(filename).getroot()
-    return [tag.attrib["type"] for tag in root.findall("tags/tag")]
-
-def parse_types(filename):
-    """Takes a filename and returns a list of all possible monster types"""
-    root = tree.parse(filename).getroot()
-    return [tag.tag for tag in root.find("types")]
 
 class Monster(character.Character):
     """The enemy characters encountered in battle. The Monster object
     is responsible for holding a Monster's stats as well as generating
     itself."""
 
-    BUILDERS = [GeneralBuilder]
-
     DATAPATH = os.path.dirname(os.path.realpath(__file__)) + "/data/"
-    TAGS = parse_tags(DATAPATH+"monster.xml")
-    TYPES = parse_types(DATAPATH+"monster.xml")
+    MONSTERS = parse_monsters(DATAPATH+"monster.xml")
     # NAMES = _XML.find("names")
     # IMAGE = _XML.find("image")
 
-    DEFAULT_RANK = {
-        "common": 90,
-        "elite": 100,
-        "boss": -1
-    }
-
-    DEFAULT_STATS = {
-        "points": 100,
-        "attack": 1,
-        "defense": 1,
-        "magic": 1,
-        "speed": 1,
-        "health": 1
-    }
-
-    def __init__(self, **parameters):
+    def __init__(self, name):
         """Basic Monster constructor"""
         super().__init__("")
-        self.builders = [] # construct builders
-        for builder in Monster.BUILDERS:
-            value = parameters.get(builder.NAME)
-            self.builders.append(builder(value))
-        self.builders = sorted(self.builders, key=lambda b: b.priority)
-
-        self.tag = self._generate_tag(Monster.TAGS)
-        self.rank = self._generate_rank(Monster.DEFAULT_RANK)
-        self.type = self._generate_type(Monster.TYPES)
-        self.name = self._generate_name(self.tag, self.rank, self.type)
-        self.stats = self._generate_stats(Monster.DEFAULT_STATS, self.tag, self.rank, self.type)
-        self.current_health = self.stats["health"]
-        self.abilities = self._generate_abilities(self.tag, self.rank, self.type)
-        self.surface = self._generate_surface()
-        self.surface = "image/monster/slime1_base.png"
-        self.hover = "image/monster/slime1_highlight.png"
-
-    def _generate_tag(self, tags):
-        """Generates the possible tag of the monster"""
-        possible_tags = list(tags) # copy
-        for builder in self.builders:
-            possible_tags = builder.build_tags(possible_tags)
-        return random.choice(possible_tags)
-
-    def _generate_type(self, types):
-        """Generates the possible type of the monster"""
-        possible_types = list(types) # copy
-        for builder in self.builders:
-            possible_types = builder.build_tags(possible_types)
-        return random.choice(possible_types)
-
-
-    def _generate_rank(self, distribution):
-        """Generates the rank of the monster randomly based on distribution"""
-        rank_distribution = distribution.copy()
-        for builder in self.builders:
-            rank_distribution = builder.build_rank(rank_distribution)
-        roll = random.randint(0, 100)
-        if roll <= rank_distribution["common"]:
-            return "common"
-        elif roll <= rank_distribution["elite"]:
-            return "elite"
-        elif roll <= rank_distribution["boss"]:
-            return "boss"
-
-    def _generate_name(self, tag, rank, type):
-        """Generates a template based on rank, and constructs name with filled in template"""
-        if rank == "common":
-            name_template = {"adj": ""}
-        elif rank == "elite":
-            name_template = {"noun": ""}
-        else:
-            name_template = {"pronoun": ""}
-        for builder in self.builders:
-            name_template = builder.build_name(name_template, tag, rank, type)
-
-        if rank == "common":
-            name = name_template["adj"] + " " + type
-        elif rank == "elite":
-            name = type + " of the " + name_template["noun"]
-        else:
-            name = name_template["pronoun"]
-
-        return name
-
-    def _generate_stats(self, stats, tag, rank, type):
-        """Generates stats based on distribution"""
-        raw_stats = stats.copy()
-        for builder in self.builders:
-            raw_stats = builder.build_stats(raw_stats, tag, rank, type)
-
-        s =   raw_stats["attack"]\
-            + raw_stats["defense"]\
-            + raw_stats["magic"]\
-            + raw_stats["speed"]\
-            + raw_stats["health"]
-        attack_dist = raw_stats["attack"] / s
-        defense_dist = raw_stats["defense"] / s
-        magic_dist = raw_stats["magic"] / s
-        speed_dist = raw_stats["speed"] / s
-        health_dist = raw_stats["health"] / s
-
-        stats = {}
-        stats["points"] = round(raw_stats["points"])
-        stats["attack"] = round(raw_stats["points"] * attack_dist)
-        stats["defense"] = round(raw_stats["points"] * defense_dist)
-        stats["magic"] = round(raw_stats["points"] * magic_dist)
-        stats["speed"] = round(raw_stats["points"] * speed_dist)
-        stats["health"] = round(raw_stats["points"] * health_dist)
-
-        return stats
-
-    def _generate_abilities(self, tag, rank, type):
-        """Generates list of monster abilities"""
-        abilities = []
-        for builder in self.builders:
-            abilities = builder.build_abilities(abilities, tag, rank, type)
-
-        return abilities
-
-
-    def _generate_surface(self):
-        pass
-        # base = ""
-        # mask = ""
-        # overlay = ""
-        # enhance = ""
-        # splash = ""
-        # if self.difficulty >= Monster.COMMON:
-        #     base = choice(Monster.IMAGE.findall("base/%s"%self.mtype)).text
-        #     mask = choice(Monster.IMAGE.findall("mask/%s"%self.mtype)).text
-        #     hover = choice(Monster.IMAGE.findall("hover/%s"%self.mtype)).text
-        #     overlay = choice(Monster.IMAGE.findall("overlay/%s"%choice(
-        #         self.tags).attrib["type"])).text
-        # if self.difficulty >= Monster.ELITE:
-        #     enhance = choice(Monster.IMAGE.findall("enhance/%s"%choice(
-        #         self.tags).attrib["type"])).text
-        # if self.difficulty == Monster.BOSS:
-        #     splash = choice(Monster.IMAGE.findall("splash/%s"%choice(
-        #         self.tags).attrib["type"])).text
-
-        # if base: base = image.load(base).convert_alpha()
-        # if mask: mask = image.load(mask).convert_alpha()
-        # if hover: hover = image.load(hover).convert_alpha()
-        # if overlay: overlay = image.load(overlay).convert_alpha()
-        # if enhance: enhance = image.load(enhance).convert_alpha()
-        # if splash: splash = image.load(splash).convert_alpha()
-
-        # tmp_surface = Surface(base.get_size(), SRCALPHA)
-        # tmp_surface.fill((0, 0, 0, 0))
-        # tmp_hover_surface = tmp_surface.copy()
-        # self.surface = Surface(base.get_size(), SRCALPHA)
-        # self.hover_surface = Surface(base.get_size(), SRCALPHA)
-        # self.surface.fill((0, 0, 0, 0))
-        # self.hover_surface.fill((0, 0, 0, 0))
-
-        # if base:
-        #     tmp_surface.blit(base, (0, 0))
-        #     tmp_hover_surface.blit(hover, (0, 0))
-        # if mask:
-        #     tmp_surface.blit(mask, (0, 0))
-        #     tmp_hover_surface.blit(mask, (0, 0))
-        # if overlay:
-        #     tmp_surface.blit(overlay, (0, 0), special_flags=BLEND_RGBA_MULT)
-        #     tmp_hover_surface.blit(overlay, (0, 0), special_flags=BLEND_RGBA_MULT)
-        #     tmp_hover_surface.blit(hover, (0, 0))
-        # if enhance:
-        #     tmp_surface.blit(enhance, (0, 0))
-        #     tmp_hover_surface.blit(enhance, (0, 0))
-        # if splash:
-        #     self.surface.blit(splash, (0, 0))
-        #     self.hover_surface.blit(splash, (0, 0))
-
-        # self.surface.blit(tmp_surface, (0, 0))
-        # self.hover_surface.blit(tmp_hover_surface, (0, 0))
-
-        # self.surface = scale(self.surface,
-        #             (self.surface.get_width()*SCALE,
-        #              self.surface.get_height()*SCALE))
-
-        # self.hover_surface = scale(self.hover_surface,
-        #             (self.hover_surface.get_width()*SCALE,
-        #              self.hover_surface.get_height()*SCALE))
-
 
 # For testing
 if __name__ == "__main__":
--- engine/ui/manager/scenario_manager.py
+++ engine/ui/manager/scenario_manager.py
@@ -7,6 +7,7 @@ import pygame
 from engine.game.monster.monster import Monster
 from engine.ui.core.manager import Manager
 from engine.ui.core.zone import Zone
+from engine.game.item.item import Item
 import engine.ui.element as element
 
 __all__ = ["ScenarioManager"]
@@ -18,22 +19,90 @@ def action_nothing(game, **kwargs):
 def action_battle(game, **kwargs):
     """Implements a battle"""
     defaults = {
-        "number" : str(random.randint(1, 4))
+        "challenge" : random.randint(8, 12), # TO FIX
+        "monsters" : None
     }
-    # May need to add additional features in the future
     defaults.update(kwargs)
-    monsters = [Monster(**defaults) for i in range(int(defaults["number"]))]
-    game.encounter = monsters
+    if defaults["monsters"] == None:
+        names = []
+        while defaults["challenge"] > 0 and len(names) < 3:
+            valid_monsters = [(name, monster["rating"])
+                for name, monster in Monster.MONSTERS.items()
+                if monster["rating"] <= defaults["challenge"] and\
+                monster["location"] == game.floor_type]
+            if not valid_monsters:
+                break #failsafe
+            name, rating = random.choice(valid_monsters)
+            names.append(name)
+            defaults["challenge"] -= rating
+        if defaults["challenge"]:
+            valid_monsters = [(name, monster["rating"])
+                for name, monster in Monster.MONSTERS.items()
+                if monster["rating"] <= defaults["challenge"] and\
+                monster["location"] == game.floor_type]
+            if valid_monsters:
+                highest_value = 0
+                for name, value in valid_monsters:
+                    if value > highest_value:
+                        highest_value = value
+            valid_names = [name for name, value in valid_monsters
+                           if value == highest_value]
+            names.append(random.choice(valid_names))
+        defaults["monsters"] = [Monster(name) for name in names]
+    else:
+        names = defaults["monsters"]
+        defaults["monsters"] = [Monster(name) for name in names]
+    monsters = [Monster(**defaults) for i in range(defaults["number"])]
+    game.encounter = defaults["monsters"]
 
 def action_loot(game, **kwargs):
-    shards = 0
-    items = []
-    for key in kwargs:
-        if key == "shard":
-            shards = int(kwargs[key])
-        elif key == "items":
-            pass
-    game.loot = (shards, items)
+    """Gives party loot"""
+    defaults = {
+        "reward-tier" : "low"
+        "shard" : None,
+        "item" : None
+    }
+    defaults.update(kwargs)
+    # Handle unspecified shard
+    if defaults["shard"] == None: # arbitrary
+        if defaults["reward-tier"] == "low":
+            defaults["shard"] = game.floor_level * random.randint(15, 20)
+        elif defaults["reward-tier"] == "medium":
+            defaults["shard"] = game.floor_level * random.randint(30, 35)
+        elif defaults["reward-tier"] == "high":
+            defaults["shard"] = game.floor_level * random.randint(50, 70)
+    # Handle unspecified item
+    if defaults["item"] == None:
+        defaults["item"] = []
+        parameters = {
+            "floor": game.floor_level,
+            "floor-type": game.floor_type
+        }
+        if "reward-tier" == "medium":
+            if random.randint(0, 99) < 50:
+                defaults.append(Item(**parameters))
+        elif "reward-tier" == "high":
+            game.floor_level * random.randint(50, 70)
+            if random.randint(0, 99) < 30:
+                defaults.append(Item(**parameters))
+    # Handle raw input as a string for item
+    else:
+        # item := <name>,<parameter>|[<more parameters>]@[<item>]
+        items = defaults["item"]
+        defaults["item"] = []
+        if items:
+            for item_parameters in items.split("@"):
+                # to be fed into the builders
+                parameters = {
+                    "floor": game.floor_level,
+                    "floor-type": game.floor_type
+                }
+                for parameter in item_parameters.split("|"):
+                    parameter_name = parameter.split(",")[0]
+                    parameter_value = parameter.split(",")[1]
+                    parameters[parameter_name] = parameter_value
+                defaults["item"].append(Item(**parameters))
+    game.loot = (defaults["shards"], defaults["item"])
     game.focus_window = "loot"
 
 ACTIONS = {
@@ -117,10 +186,10 @@ class ScenarioManager(Manager):
         game.focus_window = None
         game.selected_player = None
         # Parse action
-        action_data = dialog.action.split(" ")
-        action_type = action_data[0]
-        kwargs = {}
-        if len(action_data) > 1:
+        # action := <type> [<parameter-name>:<parameter> <para...]
+        for action in dialog.action:
+            action_data = action.split(" ")
+            action_type = action_data[0]
             kwargs = {action.split(":")[0]: action.split(":")[1] \
                 for action in action_data[1:]}
-        ACTIONS[action_type](game, **kwargs)
+            ACTIONS[action_type](game, **kwargs)
