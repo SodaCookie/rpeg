008f39fe9056393dfd76ba1e2e0791476a1fd7c3
SodaCookie <xzeric@hotmail.com>
2016-04-01 12:57:40 -0400

    Finished editor prototype layout. Created the main.py and converted design.ui to design.py

---
+++ editor/main.py
@@ -0,0 +1,22 @@
+#!python3.4
+
+from PyQt5 import QtGui, QtWidgets
+import sys
+import os
+
+import design
+
+class Editor(QtWidgets.QMainWindow, design.Ui_MainWindow):
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    form = Editor()
+    form.show()
+    app.exec_()
+
+if __name__ == '__main__':
+    main()
