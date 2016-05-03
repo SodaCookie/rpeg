c47f98f57cb21e96a76707077c4ce4d0ad61fb8e
SodaCookie <xzeric@hotmail.com>
2016-05-02 23:17:40 -0400

    Created a refactored Dialogue class created assets folders for actions and conditions

---
+++ assets/actions/BattleAction.py
@@ -0,0 +1,53 @@
+from engine.game.dungeon.action import Action
+from engine.game.monster.monster import Monster
+
+class BattleAction(Action):
+
+    def __init__(self, challenge=5, monsters=None):
+        super().__init__()
+        self.challenge = challenge
+        self.monsters = monsters
+
+    def execute(self, game):
+        """Initiates a battle.
+        If no monsters are declared for the battle then a random
+        set of monsters will be generated at the specified challenge
+        rating. Otherwise monsters will override."""
+        monster_names = []
+        if self.monsters:
+            monster_names = self.monsters
+        else:
+            challenge = self.challenge
+            while challenge > 0 and len(monster_names) < 3:
+                valid_monsters = []
+                # Get valid monsters
+                for name, monster in Monster.MONSTERS:
+                    if monster["rating"] <= defaults["challenge"] and \
+                            monster["location"] == game.floor_type and \
+                            not monster["unique"]:
+                        valid_monsters.append((name, monster["rating"]))
+                # If no valid monsters break
+                if not valid_monsters:
+                    break
+                # Select monster
+                name, rating = random.choice(valid_monsters)
+                monster_names.append(name)
+                challenge -= rating
+
+            # If there are remaining challenge points to distribute
+            if challenge: # Any rating left over
+                          # then we add the highest
+                valid_monsters = [(name, monster["rating"])
+                    for name, monster in Monster.MONSTERS.items()
+                    if monster["rating"] <= challenge and \
+                    monster["location"] == game.floor_type and \
+                    not monster["unique"]]
+
+                highest_value = 0
+                for name, value in valid_monsters:
+                    if value > highest_value:
+                        highest_value = value
+                valid_names = [name for name, value in valid_monsters
+                               if value <= highest_value]
+                monster_names.append(random.choice(valid_names))
+        game.encounter = [Monster(name) for name in monster_names]
--- engine/game/dungeon/dialog.py
+++ engine/game/dungeon/dialog.py
@@ -1,5 +1,39 @@
 import random
 
+class Dialogue(object):
+	"""Dialogue object is used to keep track of possible text"""
+
+	def __init__(self, dtext, body, chance=100, fail=None, choices=None,
+			conditions=None, actions=None):
+		"""Creates a Dialogue object.
+		name - an identifier to access the dialogue
+		body - text to display onto the screen
+		chance - chance to succeed where 100 is guaranteed
+		fail - the dialogue to call if failed
+		choices - a dictionary to access the children
+		condition - conditions to access this choice
+		action - """
+		self.dtext = dtext # dialogue text (text for the choice)
+		self.body = body
+		self.chance = chance
+		self.fail = fail
+		self.choices = choices if choices else []
+		self.conditions = conditions if conditions else []
+		self.actions = actions if actions else []
+
+	def get_available_choices(self, party):
+		"""get the available choices based on the party"""
+		available = []
+		for choice in self.choices.items():
+			if all(condition.apply(party) for condition in choice.conditions):
+				available.append(choice)
+		return available
+
+	def get_actions(self):
+		"""Return the list of actions"""
+		return self.actions
+
+
 class Dialog(object):
 	"""Dialog object is used to keep track of possible text"""
 
